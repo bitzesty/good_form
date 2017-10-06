e658835a7e7f996024b09d611d51c1d297259f0a
Matthew Ford <matt@bitzesty.com>
2017-10-06 19:54:46 +0100

    remove utf8 param

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -3,7 +3,7 @@ class FormsController < ApplicationController
   def create
     forward_params = params.permit!
     logger.info forward_params
-    FormMailer.forward_email(forward_params.to_hash.except("action", "controller")).deliver
+    FormMailer.forward_email(forward_params.to_hash.except("action", "controller", "utf8")).deliver
     redirect_to request.referer || GoodForm::Application.config.default_return_url
   end
 end
