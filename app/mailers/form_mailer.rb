b6a7cf51b096395a9f49f48884307d6097466b70
Matthew Ford <matt@bitzesty.com>
2017-10-06 19:36:14 +0100

    safe fetch

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -3,7 +3,7 @@ class FormMailer < ApplicationMailer
   def forward_email(params)
     @params = params
     mail to: GoodForm::Application.config.mailto_address,
-         reply_to: @params.fetch('email'),
+         reply_to: @params.fetch('email', nil),
          subject: GoodForm::Application.config.mailto_subject
   end
 end
