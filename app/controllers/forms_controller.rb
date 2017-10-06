0681ca8f6b003281349dd2dfce65af2fbb99ec29
Matthew Ford <matt@bitzesty.com>
2017-10-06 19:40:29 +0100

    add logging

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -2,6 +2,7 @@ class FormsController < ApplicationController
 
   def create
     forward_params = params.permit!
+    logger.info forward_params
     FormMailer.forward_email(forward_params.to_hash.except("action", "controller")).deliver
     redirect_to request.referer || GoodForm::Application.config.default_return_url
   end
