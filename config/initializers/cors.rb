92ea2470072df643ad8d80ec5d5cf728b47ec363
Maurício André Cinelli <mauricio.cinelli@bitzesty.com>
2017-10-18 10:33:11 -0200

    No extra env var needed

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins GoodForm::Application.config.domain, ENV["ALLOWED_CORS_DOMAIN"]
+    origins GoodForm::Application.config.domain
 
     resource '*',
     headers: :any,
