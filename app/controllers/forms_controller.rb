d019dcbc2fdaca00a6cd99143779c1817de4b23b
Maurício André Cinelli <mauricio.cinelli@bitzesty.com>
2017-10-18 10:37:35 -0200

    Fixing condition order

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -6,9 +6,9 @@ class FormsController < ApplicationController
     FormMailer.forward_email(forward_params.to_hash.except("action", "controller", "utf8", "sender")).deliver
 
     if request.xhr?
-      redirect_to request.referer || GoodForm::Application.config.default_return_url
-    else
       render json: {status: "ok"}, status: :ok
+    else
+      redirect_to request.referer || GoodForm::Application.config.default_return_url
     end
   end
 end
