37c5d0a9eeb8723f2d8b947ab819a1bd6fd47ced
Maurício André Cinelli <mauricio.cinelli@bitzesty.com>
2017-10-18 08:19:03 -0200

    Adding CORS support + responding to both html and json

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -3,7 +3,12 @@ class FormsController < ApplicationController
   def create
     forward_params = params.permit!
     logger.info forward_params
-    FormMailer.forward_email(forward_params.to_hash.except("action", "controller", "utf8")).deliver
-    redirect_to request.referer || GoodForm::Application.config.default_return_url
+    FormMailer.forward_email(forward_params.to_hash.except("action", "controller", "utf8", "sender")).deliver
+
+    if params["sender"] != "ajax"
+      redirect_to request.referer || GoodForm::Application.config.default_return_url
+    else
+      render json: {status: "ok"}, status: :ok
+    end
   end
 end
